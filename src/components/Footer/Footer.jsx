0e08bc7eadd986dcb701d628ef878f8295501753
lamis-menghour <lamismenghour@gmail.com>
2024-02-20 13:32:16 +0200

    Create the Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,109 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebookF,
+  faGithub,
+  faInstagram,
+  faLinkedin,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import "./footer.css";
+import { FooterLinks } from "../../constants/FooterLinks.js";
+import { Link } from "react-router-dom";
 
 function Footer() {
+  console.log(FooterLinks);
+  const footerIcons = [
+    {
+      id: 1,
+      icon: faFacebookF,
+      url: "https://www.facebook.com/official.arduino",
+    },
+    {
+      id: 2,
+      icon: faInstagram,
+      url: "https://www.instagram.com/arduino.cc/",
+    },
+    {
+      id: 3,
+      icon: faTwitter,
+      url: "https://twitter.com/arduino",
+    },
+    {
+      id: 4,
+      icon: faGithub,
+      url: "https://github.com/arduino/",
+    },
+    {
+      id: 5,
+      icon: faLinkedin,
+      url: "https://www.linkedin.com/company/arduino/",
+    },
+    {
+      id: 6,
+      icon: faYoutube,
+      url: "https://www.youtube.com/user/arduinoteam",
+    },
+  ];
   return (
-    <div>
-      
+    <div className="footer-section">
+      <div className="footer-container">
+        <div className="footer-header">
+          <div className="footer-header-left">ARDUINO</div>
+          <div className="footer-header-right">
+            <FontAwesomeIcon icon={faChevronUp} size="2xs" />
+            Back to top
+          </div>
+        </div>
+
+        <div className="footer-content">
+          <div className="footer-content-left">
+            {FooterLinks["content-left"].map((item) => (
+              <Link
+                to={item.url}
+                key={item.id}
+                className={`footer-link link${item.id}`}
+              >
+                {item.title}
+              </Link>
+            ))}
+          </div>
+          <div className="footer-content-center">
+            <div className="title">newsletter</div>
+            <div className="input-wrapper">
+              <input type="email" placeholder="Enter your email to sign up" />
+              <button type="submit">Subscribe</button>
+            </div>
+          </div>
+          <div className="footer-content-right">
+            <div className="title">follow us</div>
+            <div className="footer-icons">
+              {footerIcons.map((item) => (
+                <Link to={item.url} key={item.id} className="footer-icon">
+                  <FontAwesomeIcon icon={item.icon} size="md" />
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="footer-bottom">
+          <Link to="/" className="copy-right footer-link">
+            © 2024 Arduino
+          </Link>
+          <div className="footer-links">
+            {FooterLinks.bottom.map((item) => (
+              <Link to={item.url} key={item.id} className="footer-link">
+                {item.title}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
---
+++ src/constants/FooterLinks.js
@@ -0,0 +1,57 @@
+export const FooterLinks = {
+  "content-left": [
+    {
+      id: 1,
+      title: "Trademark",
+      url: "/",
+    },
+    {
+      id: 2,
+      title: "Contact Us",
+      url: "/",
+    },
+    {
+      id: 3,
+      title: "Distributors",
+      url: "/",
+    },
+    {
+      id: 4,
+      title: "Careers",
+      url: "/",
+    },
+    {
+      id: 5,
+      title: "Help Center",
+      url: "/",
+    },
+    {
+      id: 6,
+      title: "Whistleblowing",
+      url: "/",
+    },
+  ],
+
+  bottom: [
+    {
+      id: 1,
+      title: "Terms Of Service",
+      url: "/",
+    },
+    {
+      id: 2,
+      title: "Privacy Policy",
+      url: "/",
+    },
+    {
+      id: 3,
+      title: "Security",
+      url: "/",
+    },
+    {
+      id: 4,
+      title: "Cookie Settings",
+      url: "/",
+    },
+  ],
+};
